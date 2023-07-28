077fc676b8864cf6ed5997693ba94f2bcb7e4c1b
LeeC0X <Leecox237@gmail.com>
2023-07-28 18:39:29 -0400

    UpdatedMemeCreator

--- assessments/memegenerator.js
+++ assessments/memegenerator.js
@@ -1,2 +1,41 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const memeForm = document.getElementById("meme-form");
 
+  memeForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
+    const imageUrl = document.getElementById("image-url").value;
+    const topText = document.getElementById("top-text").value;
+    const bottomText = document.getElementById("bottom-text").value;
+
+    let memeHolder = document.createElement("div");
+    memeHolder.classList.add("meme");
+    memeHolder.addEventListener("click", function (event) {
+      memeHolder.remove();
+    });
+
+    let image = document.createElement("img");
+    image.src = imageUrl;
+
+    let topTextElement = document.createElement("div");
+    topTextElement.classList.add("text", "top-text");
+    topTextElement.textContent = topText;
+
+    let bottomTextElement = document.createElement("div");
+    bottomTextElement.classList.add("text", "bottom-text");
+    bottomTextElement.textContent = bottomText;
+
+    memeHolder.appendChild(image);
+    memeHolder.appendChild(topTextElement);
+    memeHolder.appendChild(bottomTextElement);
+
+    let memeHolderDiv = document.getElementById("meme-holder");
+    memeHolderDiv.appendChild(memeHolder);
+
+    document.getElementById("image-url").value = "";
+    document.getElementById("top-text").value = "";
+    document.getElementById("bottom-text").value = "";
+
+    let memeHolderRemove = document.getElementById("meme-holder");
+  });
+});
