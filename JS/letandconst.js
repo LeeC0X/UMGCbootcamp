bedae992a40a2e4219ec04f55381d5972314486e
LeeC0X <Leecox237@gmail.com>
2023-11-29 16:06:39 -0500

    letconstquiz

---
+++ JS/letandconst.js
@@ -0,0 +1,7 @@
+// let can be reassigned, but it can't be redeclared. Var can be redeclared.
+
+// const cannot be reassigned or redeclared when used. var can
+
+// let can be reassigned, but const and neither be redeclared or reassigned.
+
+// hoisting is when a variable is lifted to the top of the scope it is declared in.
