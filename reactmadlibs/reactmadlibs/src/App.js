5213bc1482c2624b646090b8226ccd92fadb574e
LeeC0X <Leecox237@gmail.com>
2024-04-18 18:11:32 -0400

    assessment

---
+++ reactmadlibs/reactmadlibs/src/App.js
@@ -0,0 +1,12 @@
+import NewMadLibForm from "./NewMadLibForm";
+import MadLibBoard from "./MadLibBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <MadLibBoard />
+    </div>
+  );
+}
+
+export default App;
